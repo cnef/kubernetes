4c55394d57a6c219c7e438f8819bfdaa5fe2f76a
root <kxu1983@gmail.com>
2019-04-27 08:44:43 +0800

    fix node update and syncloop race condition

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -1381,6 +1381,9 @@ func (kl *Kubelet) Run(updates <-chan kubetypes.PodUpdate) {
 
 	if kl.kubeClient != nil {
 		// Start syncing node status immediately, this may set up things the runtime needs to run.
+		glog.Warning("Make sure node status updated to avoid race condition - start")
+		kl.syncNodeStatus()
+		glog.Warning("Make sure node status updated to avoid race condition - done")
 		go wait.Until(kl.syncNodeStatus, kl.nodeStatusUpdateFrequency, wait.NeverStop)
 	}
 	go wait.Until(kl.syncNetworkStatus, 30*time.Second, wait.NeverStop)
